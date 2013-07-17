1d229c590cc3fe1e043584b5e72ce69b05fffa94
mackmgg <github@mack.im>
2013-07-17 12:55:28 -0400

    Create bot.php
    Uploaded bot

---
+++ bot.php
@@ -0,0 +1,44 @@
+<?php
+	$access_token = '';                                         // Generate at http://ovh.mack.im/fb.php 
+	$username = '';                                             // Facebook username (or ID number)
+	$database = '';                                             // MySQL database for storing replies
+	$dbuser = '';                                               // MySQL username
+	$dbpass = '';                                               // MySQL password
+	$timedelay = 10;                                            // Delay between checks (in seconds)
+	$message = 'Thanks!';                                       // Message to reply to people
+	$regex = '/(b(irth|-)(day)?)|((h)?bd)|(b(irth|-)?day)/i';   // Regex to determine birthday messages
+	
+	for(;;) {
+	  $feed = file_get_contents("https://graph.facebook.com/".$username."/feed?access_token=".$access_token);
+	  $data = json_decode($feed);
+	  foreach($data->data as $datas) {
+	    if(isset($datas->message)) {
+	      if(preg_match($regex,$datas->message)) {
+		        $db = new PDO("mysql:host=localhost;dbname=".$database,$dbuser,$dbpass);
+		        $statement = $db->prepare("SELECT fbid FROM replies WHERE fbid = ?");
+		        $statement->execute(array($datas->id));
+		        if($statement->fetch()==FALSE) {
+		          echo "Not Found! ".$datas->message."\n";
+		          $qry = $db->prepare("INSERT INTO replies (fbid, message, time) VALUES (?, ?, ?)");
+	          $qry->execute(array($datas->id, $datas->message, time()));
+	          $curlurl = "https://graph.facebook.com/".$datas->id."/comments\n";
+	          $attachment = array(
+	            'access_token' => $access_token,
+	            'message'      => $message);
+	          $ch = curl_init();
+	          curl_setopt($ch, CURLOPT_URL, $curlurl);
+	          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	          curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+	          curl_setopt($ch, CURLOPT_POST, true);
+	          curl_setopt($ch, CURLOPT_POSTFIELDS, $attachment);
+	          curl_setopt($ch, CURLOPT_HEADER, 0);
+	          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	          $comment = curl_exec($ch);
+	          curl_close ($ch);
+	        }
+	      }
+	    }
+	  }
+	  sleep($timedelay);
+	}
+?>
